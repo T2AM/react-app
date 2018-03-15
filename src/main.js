de5f52aaefe0b8ebc2a8db77a35a7f57360942a4
ckingbailey <ckingbailey@gmail.com>
2018-03-15 12:58:57 -0700

    wire up a route from Main

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import FrontPage from 'front_page';
+import Profile from 'profile';
 
-export default class Main extends Component {
-  render() {
-    return (
-      <div class="main">Main content</div>
-    )
-  }
+const Main = () => {
+  return (
+    <Switch>
+      <Route path="/frontpage" component="FrontPage" />
+      <Route path="/profile/:id" component="Profile" />
+    </Switch>
+  )
 }
