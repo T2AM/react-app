bfbb3072b7e433bbad23f3d05e1aebafa9b28878
ckingbailey <ckingbailey@gmail.com>
2018-03-21 15:19:45 -0700

    add some placeholder profile data

--- src/profile.js
+++ src/profile.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+// this component receives its id from the `match` prop
+// so then what--does the component use `id` to qry the db for user's deets?
+// I guess that means Profile manages its own state. yech
 const Profile = props => (
   <div className="profile">
-    <h3>Greetings, you've arrived at the profile page</h3>
-    <p>{ props.match.params.id || '000000' }</p>
+    <img
+      className="maintainer-avatar"
+      src="https://upload.wikimedia.org/wikipedia/commons/c/c0/Ada_Lovelace_Chalon_portrait.jpg"
+    />
+    <h3 className="maintainer-name">Ada Lovelace</h3>
+    <h4>{ props.match.params.id }</h4>
+    <p className="maintainer-blurb">blurb I maintain The Internet lorem ipsum consecutur</p>
+    <div className="badges">
+      <img className="badge" src="path/to/server/webpack_badge.png"/>
+      <img className="badge" src="path/to/server/css_badge.png"/>
+      <img className="badge" src="path/to/server/django_badge.png"/>
+    </div>
   </div>
 )
 
