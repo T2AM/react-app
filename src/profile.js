5726bd92c5fc97a2b30fb15d092e59dc396e16ac
Colin <ckingbailey@gmail.com>
2018-04-25 19:44:07 -0700

    Profile page (#2)
    * add some placeholder profile data
    
    * provide src for profile badges imgs
    
    * gitignore assets folder
    
    * add Projects components and then realize I ought to make a new branch

--- src/profile.js
+++ src/profile.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+// this component receives its id from the `match` prop
+// so then what--does the component use `id` to qry the db for user's deets?
+// I guess that means Profile manages its own state. yech
 const Profile = props => (
   <div className="profile">
-    <h3>Greetings, you've arrived at the profile page</h3>
-    <p>{ props.match.params.id || '000000' }</p>
+    <img
+      className="maintainer-avatar"
+      src="https://upload.wikimedia.org/wikipedia/commons/c/c0/Ada_Lovelace_Chalon_portrait.jpg"
+      alt="maintainer avatar"
+    />
+    <h3 className="maintainer-name">Ada Lovelace</h3>
+    <h4>{ props.match.params.id }</h4>
+    <p className="maintainer-blurb">blurb I maintain The Internet lorem ipsum consecutur</p>
+    <div className="badges">
+      <img className="badge" src="https://webpack.js.org/assets/icon-square-big.svg" alt="webpack badge"/>
+      <img className="badge" src="http://marioaraque.com/sites/default/files/styles/large/public/field/image/css3.png" alt="css3 badge"/>
+      <img className="badge" src="http://flask.pocoo.org/static/logo/flask.svg" alt="flask badge"/>
+    </div>
   </div>
 )
 
